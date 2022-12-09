f2568e835f1a7fcb85dba7bd00ee9113e7d2450b
HugoNicolau <nicolau.hugogiles@gmail.com>
2022-12-09 07:37:27 -0300

    Queries 08, 09 and 10;

---
+++ Prática - Exercícios SQL/08.sql
@@ -0,0 +1,1 @@
+UPDATE clientes SET cpf='04652651298' WHERE nome='Lucca Santarém Branco';
---
+++ Prática - Exercícios SQL/09.sql
@@ -0,0 +1,4 @@
+SELECT id FROM clientes WHERE nome='Olga Cascais Fortunato';
+SELECT id FROM produtos WHERE nome='Celular Topo de Linha';
+INSERT INTO "compras" ("id_cliente", "id_produto") VALUES ('4', '8');
+INSERT INTO "compras" ("id_cliente", "id_produto") VALUES ('4', '8');
