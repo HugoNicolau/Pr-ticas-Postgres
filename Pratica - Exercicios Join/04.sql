1cb3999c3be7b5aa2719f185ad232f73bf4e5df6
HugoNicolau <nicolau.hugogiles@gmail.com>
2022-12-14 19:46:52 -0300

    Join tables, renaming columns and "is null" use

---
+++ Pratica - Exercicios Join/04.sql
@@ -0,0 +1,1 @@
+SELECT users.id, users.name, roles.name AS role, companies.name AS company, experiences."startDate" FROM users JOIN experiences ON experiences."userId"=users.id JOIN companies ON experiences."companyId"=companies.id JOIN roles ON experiences."roleId"=roles.id WHERE users.id=50 AND "endDate" IS NULL;
