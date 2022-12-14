36cf254404ace950b69a9d44abe628ab0b167ed8
HugoNicolau <nicolau.hugogiles@gmail.com>
2022-12-14 19:23:28 -0300

    Double join with the same table using alias

---
+++ Pratica - Exercicios Join/02.sql
@@ -0,0 +1,1 @@
+SELECT testimonials.id, users1.name AS writer, users2.name AS recipient, testimonials.message FROM testimonials JOIN users users1 ON testimonials."writerId"=users1.id JOIN users users2 ON testimonials."recipientId"=users2.id;
