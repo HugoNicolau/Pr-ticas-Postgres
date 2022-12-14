6da97df0bfe8653661f7b7ee8a9e1853e4359d42
HugoNicolau <nicolau.hugogiles@gmail.com>
2022-12-14 19:33:50 -0300

    Triple join with 2 conditionals and 4 tables

---
+++ Pratica - Exercicios Join/03.sql
@@ -0,0 +1,1 @@
+SELECT users.id, users.name, courses.name AS course, schools.name AS school, educations."endDate" FROM users JOIN educations ON users.id=educations."userId" JOIN courses ON educations."courseId"=courses.id JOIN schools ON educations."schoolId"=schools.id WHERE users.id=30 AND educations.status='finished';
